bd33641cd9ce162c3d7085475746dafa17505005
Brijesh Gupta <104207113+Brijesh-Gupta-prog@users.noreply.github.com>
2024-08-09 19:00:49 +0530

    todat

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const ownerModel = require("../models/owner-model");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Hay ownwe");
 });
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async (req, res) => {
+    let owners = await ownerModel.find();
+    if (owners.length > 0) {
+      return res
+        .status(500)
+        .send("You don't have permission to create new owner");
+    }
+    let { fullname, email, password } = req.body;
+    let createdOwner = await ownerModel.create({
+      fullname,
+      email,
+      password,
+    });
+
+    res.status(201).send(createdOwner);
+  });
+}
 
 module.exports = router;
