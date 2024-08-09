91d298f0ef5aa0f94755811e4e71228d7a1cf294
Brijesh Gupta <104207113+Brijesh-Gupta-prog@users.noreply.github.com>
2024-08-09 17:45:20 +0530

    initial post

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const path = require("path");
+
+const db = require("./config/mongoose-connection");
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+app.listen(3000);
---
+++ models/owner-model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const ownerSchema = mongoose.Schema({
+  fullname: {
+    type: String,
+    minLength: 3,
+    trim: true,
+  },
+  email: String,
+  password: String,
+  contact: Number,
+  picture: String,
+  gstin: String,
+});
+
+module.exports = mongoose.model("owner", ownerSchema);
